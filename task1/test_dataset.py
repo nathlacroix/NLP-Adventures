68d1a674cf21275d9beb19613154c832b14bbd86
Paul-Edouard Sarlin <paul.edouard.sarlin@gmail.com>
2018-04-13 16:37:25 +0200

    Add prediction set + minor fixes

--- task1/test_dataset.py
+++ task1/test_dataset.py
@@ -3,25 +3,25 @@ from dataset import get_dataset
 
 
 if __name__ == '__main__':
-    base_path = Path('data/')
-    train_path = Path(base_path, 'sentences.train')
-    test_path = Path(base_path, 'sentences.eval')
-
+    data_path = Path('data/')
     config = {
-            'dictionary_size': 20000,
+            'vocab_size': 20000,
             'validation_size': 100,
-            'setence_length': 30
+            'sentence_size': 30
     }
 
-    vocab, train_data, val_data, test_data = get_dataset(
-            train_path, test_path, **config)
+    vocab, train_data, val_data, test_data, pred_data = get_dataset(data_path, **config)
 
-    assert len(vocab) == 20000
-    assert len(val_data) == 100
+    assert len(vocab) == config['vocab_size']
+    assert len(val_data) == config['validation_size']
     for data in [train_data, val_data, test_data]:
         for sentence in data:
-            assert len(sentence) == 30
+            assert len(sentence) == config['sentence_size']
             assert vocab[sentence[0]] == '<bos>'
             assert vocab[sentence[-1]] in ['<eos>', '<pad>']
+
+    for sentence in pred_data:
+        assert len(sentence) < config['sentence_size']
+        assert vocab[sentence[0]] == '<bos>'
 
     print('Dataset test passed.')
