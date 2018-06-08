572b353e20111bd40d2f08e2dbfc8da7b5974bc6
rpautrat <remi.pautrat@polytechnique.org>
2018-06-08 17:52:40 +0200

    Set up env

--- utils/set_env.sh
+++ utils/set_env.sh
@@ -1,14 +1,12 @@
 env_dir=./env
 python_version="3.6.1"
-
-module load eth_proxy python_gpu/$python_version cuda/9.0.176 cudnn/7.0
-module load jdk maven  # for cogcomp/PathLSTM
+python_path=$(which python)
 
 if [ ! -d "$env_dir" ]; then
     python -m pip install --user virtualenv
     python -m virtualenv \
         --system-site-packages \
-        --python="/cluster/apps/python/$python_version/bin/python" \
+        --python=python_path \
         "$env_dir"
 fi
 
